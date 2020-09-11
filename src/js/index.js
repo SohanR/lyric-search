6f281a23588b04af5020b2e015518498ab4ece55
SohanR <mizan.rahman66d@gmail.com>
2020-09-11 21:56:16 +0600

    adding fetch results to dom

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,37 @@ const more = document.getElementById("more");
 
 const apiUrl = "https://api.lyrics.ovh";
 
+search.focus();
+
+//fetch song from api(search song)
+function searchSong(term) {
+  fetch(`${apiUrl}/suggest/${term}`)
+    .then((res) => res.json())
+    .then((data) => {
+      showData(data);
+    });
+}
+
+//showing data in DOM
+function showData(data) {
+  console.log(data);
+
+  result.innerHTML = `
+        <ul class="songs">
+            ${data.data
+              .map(
+                (song) => `
+                    <li>
+                        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+                        <button class="btn btn-primary" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+                    </li>
+                `
+              )
+              .join("")}
+        </ul>
+    `;
+}
+
 //event listeners
 
 form.addEventListener("submit", (e) => {
